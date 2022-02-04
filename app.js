473b9d19214081eddb903841afa6b2f185a74253
Joseph <softjoe40@gmail.com>
2022-02-04 14:50:34 +0100

    create signup page

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
 
 
 app.listen(2020, () => {
